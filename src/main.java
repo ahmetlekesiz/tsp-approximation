1573854d674cdf5f75c6fbc0bd664c8d20236c04
Ahmet Lekesiz <ahmetlekesiz@gmail.com>
2020-06-13 23:58:10 +0300

    read input from text file

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<int[]> cityList = new ArrayList<int[]>();
+	
+		// Read input file line by line by using scanner
+		try {
+			Scanner scanner = new Scanner(new File("./inputs/example-input-1.txt"));
+			while (scanner.hasNextLine()) {
+				String[] arr = scanner.nextLine().split(" ");
+				cityList.add(new int[] {Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Integer.parseInt(arr[2])});
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println();
+	}
+
+}
